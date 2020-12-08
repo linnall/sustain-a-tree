5ce62f28285221b1824146772541267ae3f0a41a
Linna Luo <linnaluo18@gmail.com>
2020-12-07 23:08:13 -0500

    code refactor

--- app.js
+++ app.js
@@ -1,172 +1,127 @@
-const plus1 = document.querySelector('#plus-button-1');
-const fill1 = document.querySelector('#fill-1');
-const count1 = document.querySelector('#count-1')
-const minus1 = document.querySelector('#minus-button-1');
-const reset1 = document.querySelector('#reset-1'); 
+//Progress bar objects
+const waterBottleBar = {
+    plusBtn: document.querySelector('#plus-button-1'),
+    minusBtn: document.querySelector('#minus-button-1'),
+    resetBtn: document.querySelector('#reset-1'),
+    fill: document.querySelector('#fill-1'),
+    countDisplay: document.querySelector('#count-1'),
+    size: 0,
+    counter: 0
+}
 
-const plus2 = document.querySelector('#plus-button-2');
-const fill2 = document.querySelector('#fill-2');
-const count2 = document.querySelector('#count-2')
-const minus2 = document.querySelector('#minus-button-2');
-const reset2 = document.querySelector('#reset-2'); 
+const groceryBagBar = {
+    plusBtn: document.querySelector('#plus-button-2'),
+    minusBtn: document.querySelector('#minus-button-2'),
+    resetBtn: document.querySelector('#reset-2'),
+    fill: document.querySelector('#fill-2'),
+    countDisplay: document.querySelector('#count-2'),
+    size: 0,
+    counter: 0
+}
 
-const plus3 = document.querySelector('#plus-button-3');
-const fill3 = document.querySelector('#fill-3');
-const count3 = document.querySelector('#count-3')
-const minus3 = document.querySelector('#minus-button-3');
-const reset3 = document.querySelector('#reset-3'); 
+const refusingBar = {
+    plusBtn: document.querySelector('#plus-button-3'),
+    minusBtn: document.querySelector('#minus-button-3'),
+    resetBtn: document.querySelector('#reset-3'),
+    fill: document.querySelector('#fill-3'),
+    countDisplay: document.querySelector('#count-3'),
+    size: 0,
+    counter: 0
+}
 
-const plus4 = document.querySelector('#plus-button-4');
-const fill4 = document.querySelector('#fill-4');
-const count4 = document.querySelector('#count-4')
-const minus4 = document.querySelector('#minus-button-4');
-const reset4 = document.querySelector('#reset-4'); 
+const compostBar = {
+    plusBtn: document.querySelector('#plus-button-4'),
+    minusBtn: document.querySelector('#minus-button-4'),
+    resetBtn: document.querySelector('#reset-4'),
+    fill: document.querySelector('#fill-4'),
+    countDisplay: document.querySelector('#count-4'),
+    size: 0,
+    counter: 0
+}
 
-let size1 = 0;
-let counter1 = 0;
+// === reusable functions ===
+function clickPlus(bar) {
 
-let size2 = 0;
-let counter2 = 0;
+    if(!(bar.counter % 10)){
+        bar.size = 9.75;
+    }else {
+        bar.size += 9.75;
+    }
+    bar.counter += 1; 
 
-let size3 = 0;
-let counter3 = 0;
+    bar.fill.style.width = `${bar.size}%`;
+    bar.countDisplay.innerHTML = `${bar.counter}`;
+} 
 
-let size4 = 0;
-let counter4 = 0;
+function clickMinus(bar) {
+    if(bar.counter >= 10 && !((bar.counter-1) % 10)){
+        bar.size = 97.5;
+        bar.counter--;
+    }else if(bar.counter > 0){
+        bar.size -= 9.75;
+        bar.counter--;
+    }
+
+    bar.fill.style.width = `${bar.size}%`;
+    bar.countDisplay.innerHTML = `${bar.counter}`;
+}
+
+function clickReset(bar) {
+    bar.size = 0;
+    bar.counter = 0;
+    bar.fill.style.width = `${bar.size}%`;
+    bar.countDisplay.innerHTML = `${bar.counter}`;
+}
 
 //=========================================
-//Reusable Water Bottle
-plus1.onclick = function () {
+//Reusable Water Bottle Bar
+waterBottleBar.plusBtn.addEventListener('click', function (){
+    clickPlus(waterBottleBar);
+});
 
-    if(!(counter1 % 10)){
-        size1 = 9.75;
-    }else {
-        size1 += 9.75;
-    }
-    counter1 += 1; 
+waterBottleBar.minusBtn.addEventListener('click', function () {
+    clickMinus(waterBottleBar);
+});
 
-    fill1.style.width = `${size1}%`;
-    count1.innerHTML = `${counter1}`;
-}
+waterBottleBar.resetBtn.addEventListener('click', function () {
+    clickReset(waterBottleBar);
+});
 
-minus1.onclick = function () {
-    if(counter1 >= 10 && !((counter1-1) % 10)){
-        size1 = 97.5;
-        counter1--;
-    }else if(counter1 > 0){
-        size1 -= 9.75;
-        counter1--;
-    }
+//Grocery Bag Bar
+groceryBagBar.plusBtn.addEventListener('click', function (){
+    clickPlus(groceryBagBar);
+});
 
-    fill1.style.width = `${size1}%`;
-    count1.innerHTML = `${counter1}`;
-}
+groceryBagBar.minusBtn.addEventListener('click', function () {
+    clickMinus(groceryBagBar);
+});
 
-reset1.onclick = function () {
-    size1 = 0;
-    counter1 = 0;
-    fill1.style.width = `${size1}%`;
-    count1.innerHTML = `${counter1}`;
-}
-//=========================================
-//Reusable Grocery Bag
-plus2.onclick = function () {
+groceryBagBar.resetBtn.addEventListener('click', function () {
+    clickReset(groceryBagBar);
+});
 
-    if(!(counter2 % 10)){
-        size2 = 9.75;
-    }else {
-        size2 += 9.75;
-    }
-    counter2 += 1; 
+//Refusing Bar
+refusingBar.plusBtn.addEventListener('click', function (){
+    clickPlus(refusingBar);
+});
 
-    fill2.style.width = `${size2}%`;
-    count2.innerHTML = `${counter2}`;
-}
+refusingBar.minusBtn.addEventListener('click', function () {
+    clickMinus(refusingBar);
+});
 
-minus2.onclick = function () {
-    if(counter2 >= 10 && !((counter2-1) % 10)){
-        size2 = 97.5;
-        counter2--;
-    }else if(counter2 > 0){
-        size2 -= 9.75;
-        counter2--;
-    }
+refusingBar.resetBtn.addEventListener('click', function () {
+    clickReset(refusingBar);
+});
 
-    fill2.style.width = `${size2}%`;
-    count2.innerHTML = `${counter2}`;
-}
+//Compost Bar
+compostBar.plusBtn.addEventListener('click', function (){
+    clickPlus(compostBar);
+});
 
-reset2.onclick = function () {
-    size2 = 0;
-    counter2 = 0;
-    fill2.style.width = `${size2}%`;
-    count2.innerHTML = `${counter2}`;
-}
-//=========================================
-//Refusing Sign
-plus3.onclick = function () {
+compostBar.minusBtn.addEventListener('click', function () {
+    clickMinus(compostBar);
+});
 
-    if(!(counter3 % 10)){
-        size3 = 9.75;
-    }else {
-        size3 += 9.75;
-    }
-    counter3 += 1; 
-
-    fill3.style.width = `${size3}%`;
-    count3.innerHTML = `${counter3}`;
-}
-
-minus3.onclick = function () {
-    if(counter3 >= 10 && !((counter3-1) % 10)){
-        size3 = 97.5;
-        counter3--;
-    }else if(counter3 > 0){
-        size3 -= 9.75;
-        counter3--;
-    }
-
-    fill3.style.width = `${size3}%`;
-    count3.innerHTML = `${counter3}`;
-}
-
-reset3.onclick = function () {
-    size3 = 0;
-    counter3 = 0;
-    fill3.style.width = `${size3}%`;
-    count3.innerHTML = `${counter3}`;
-}
-//=========================================
-//Compost
-plus4.onclick = function () {
-
-    if(!(counter4 % 10)){
-        size4 = 9.75;
-    }else {
-        size4 += 9.75;
-    }
-    counter4 += 1; 
-
-    fill4.style.width = `${size4}%`;
-    count4.innerHTML = `${counter4}`;
-}
-
-minus4.onclick = function () {
-    if(counter4 >= 10 && !((counter4-1) % 10)){
-        size4 = 97.5;
-        counter4--;
-    }else if(counter4 > 0){
-        size4 -= 9.75;
-        counter4--;
-    }
-
-    fill4.style.width = `${size4}%`;
-    count4.innerHTML = `${counter4}`;
-}
-
-reset4.onclick = function () {
-    size4 = 0;
-    counter4 = 0;
-    fill4.style.width = `${size4}%`;
-    count4.innerHTML = `${counter4}`;
-}
+compostBar.resetBtn.addEventListener('click', function () {
+    clickReset(compostBar);
+});
